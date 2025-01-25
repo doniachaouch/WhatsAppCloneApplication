fe3c71f4a27579415e146d01ba1a27064337c2eb
CHAOUCH Donia <dchaouch01@gmail.com>
2025-01-25 20:28:37 +0100

    first commit

---
+++ whatsappclone/src/test/java/com/example/whatsappclone/WhatsAppClobeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whatsappclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppClobeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
