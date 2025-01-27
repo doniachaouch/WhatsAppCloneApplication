362e6b3e10e79b9917ccd9f69c81b983c0d6a437
CHAOUCH Donia <dchaouch01@gmail.com>
2025-01-27 17:50:11 +0100

    correct annotation

--- whatsappclone/src/main/java/com/example/whatsappclone/WhatsAppClobeApiApplication.java
+++ whatsappclone/src/main/java/com/example/whatsappclone/WhatsAppClobeApiApplication.java
@@ -2,6 +2,7 @@ package com.example.whatsappclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
